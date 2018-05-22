e7b54d14c25342f17e78f1ce9e7c732b3faff89f
taijuF <fukaya@camobile.com>
2018-05-22 16:49:59 +0900

    Added oauth

--- sample/client/EDAMTest.py
+++ sample/client/EDAMTest.py
@@ -14,10 +14,27 @@ if auth_token == "your developer token":
           "https://sandbox.evernote.com/api/DeveloperToken.action")
     exit(1)
 
-sandbox=True
-china=False
 
-client = EvernoteClient(token=auth_token, sandbox=sandbox,china=china)
+
+client = EvernoteClient(
+    token=auth_token
+    )
+
+# client = EvernoteClient(
+#         consumer_key="kawashima-r279-0184",
+#         consumer_secret="ac643337195c7620"
+#     )
+
+# token = client.get_request_token("http://localhost:5000")
+# print(token.get('oauth_token'))
+# print(token.get('oauth_token_secret'))
+# print(client.get_authorize_url(token))
+# oauth_token = 
+# oauth_token_secret = 
+# oauth_verifier = 
+
+# access = client.get_access_token_dict(oauth_token,oauth_token_secret,oauth_verifier)
+# print(access)
 
 user_store = client.get_user_store()
 note_store = client.get_note_store()
@@ -26,17 +43,18 @@ notebooks = note_store.listNotebooks()
 for notebook in notebooks:
     #print(notebook)
     pass
-
 note_filter = NoteStore.NoteFilter()
 note_filter.words = ''
 notes_metadata_result_spec = NoteStore.NotesMetadataResultSpec()
 notes_metadata_result_spec.includeAttributes=True
 
 count = note_store.findNoteCounts(note_filter,False)
+# print(count)
 num = 0
 for value in count.notebookCounts.values():
     num += value
 
+print(num)
 MAX = 250
 offset = 0
 
